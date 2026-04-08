49606d0f2809e99b500fe315d6526f2064f7084f
Hunter Scholz <scholzh@oregonstate.edu>
2026-04-08 12:16:47 -0700

    init proj 2.1

---
+++ week2/project2.1/ticketlock.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdatomic.h>
+
+#define MAX_THREADS 20
+
+// TODO Make global lock variable(s) here.
+
+/**
+ * Acquire the lock.
+ *
+ * `tid` is the thread ID number. You only need this to print out the
+ * required output.
+ */
+void lock(int tid) {
+    // TODO
+
+    // Print out the number of the ticket that was just grabbed:
+    //printf("Thread %d: Grabbed ticket %d\n", tid, /* ... */);
+
+    // TODO
+}
+
+/**
+ * Unlock the lock.
+ */
+void unlock(void) {
+    // TODO
+}
+
+/**
+ * Run function for all threads.
+ */
+void *run(void *args)
+{
+    int id = *(int*)args;
+
+    printf("Thread %d: Locking\n", id);
+    lock(id);
+
+    printf("Thread %d: Running\n", id);
+    usleep(50000); // 50 ms
+
+    printf("Thread %d: Unlocking\n", id);
+    unlock();
+
+    return NULL;
+}
+
+/**
+ * Main.
+ */
+int main(int argc, char *argv[])
+{
+    pthread_t t[MAX_THREADS];
+    int id[MAX_THREADS];
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: ticketlock threadcount\n");
+        return 1;
+    }
+
+    int thread_count = atoi(argv[1]);
+
+    if (thread_count < 1 || thread_count > MAX_THREADS) {
+        fprintf(stderr, "ticketlock: thread count must be betweeen 1 and %d\n",
+            MAX_THREADS);
+        return 2;
+    }
+
+    for (int i = 0; i < thread_count; i++) {
+        id[i] = i;
+        pthread_create(t + i, NULL, run, id + i);
+    }
+
+    for (int i = 0; i < thread_count; i++) {
+        pthread_join(t[i], NULL);
+    }
+}
